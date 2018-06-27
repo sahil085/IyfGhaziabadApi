1a1c12736efa8abb64622ffeae4906404956a05c
sahil085 <vermasahil269@gmail.com>
2018-06-27 23:03:33 +0530

    adding seminar controller for admin

---
+++ src/main/java/com/IyfGZB/controller/AdminSeminarController.java
@@ -0,0 +1,38 @@
+package com.IyfGZB.controller;
+
+
+import com.IyfGZB.domain.Seminar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminSeminarController {
+
+    public static final Logger logger = LoggerFactory.getLogger(AccountController.class);
+
+    @GetMapping("/seminarlist/{createdBy}")
+    public List<Seminar> getAllSeminar(@PathVariable("createdBy") String createdBy)
+    {
+
+    }
+
+    @PostMapping("/createseminar")
+    public ResponseEntity<?> createSeminar()
+    {
+        try{
+
+        }catch (Exception e){
+            logger.error(e.getMessage());
+
+        }
+
+    }
+
+
+
+}
